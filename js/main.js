b94cdfd27e76f9f0ea9050d0983b1f1f91e366da
castilloabraham <adcdabrahamcastillo@gmail.com>
2019-09-24 07:12:33 -0400

    version 1.1 - últimos cambios - casi listo

--- js/main.js
+++ js/main.js
@@ -16,15 +16,7 @@
 
 	///////////////////////////
 	// Smooth scroll
-	$("#nav .main-nav a[href^='#']").on('click', function(e) {
-		e.preventDefault();
-		var hash = this.hash;
-		$('html, body').animate({
-			scrollTop: $(this.hash).offset().top
-		}, 600);
-	});
-
-	$('#back-to-top').on('click', function(){
+		$('#back-to-top').on('click', function(){
 		$('body,html').animate({
 			scrollTop: 0
 		}, 600);
@@ -34,12 +26,6 @@
 	// Btn nav collapse
 	$('#nav .nav-collapse').on('click', function() {
 		$('#nav').toggleClass('open');
-	});
-
-	///////////////////////////
-	// Mobile dropdown
-	$('.has-dropdown a').on('click', function() {
-		$(this).parent().toggleClass('open-drop');
 	});
 
 	///////////////////////////
