b163335acb77cb4e29e5c4972c2ceaacaf68c519
castilloabraham <adcdabrahamcastillo@gmail.com>
2020-08-08 21:05:42 -0400

    version 1.3

---
+++ envia.php
@@ -0,0 +1,22 @@
+﻿<?php  
+
+// Llamando a los campos
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+
+// Datos para el correo
+$destinatario = "adcdabrahamcastillo@gmail.com";
+$asunto = "Contacto desde nuestra web";
+
+$carta = "De: $nombre \n";
+$carta .= "Correo: $correo \n";
+$carta .= "Asunto: $asunto \n";
+$carta .= "Mensaje: $mensaje";
+
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta);
+header('contacto.html');
+
+?>
